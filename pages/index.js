36bedb4e0629a55c09344fcd06d0db426e2f1265
Giuseppe Cosentino <85801475+joeycose@users.noreply.github.com>
2023-02-19 13:27:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-/*********************************************************************************
-* BTI425 – Assignment 3
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Giuseppe Cosentino Student ID: 184801215 Date: February 16, 2023
-*
-*
-********************************************************************************/
-
 import { React, useState, useEffect } from 'react';
 import useSWR from 'swr'
 import { Pagination, Accordion } from 'react-bootstrap';
